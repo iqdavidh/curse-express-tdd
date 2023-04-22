322fb6eb6e81645659fed5ac9efd8deb4a24f288
davidh <iq.davidh@gmail.com>
2023-04-22 15:23:12 -0600

    adding a new text for getAll

--- tests/integration/todoController.integration.test.js
+++ tests/integration/todoController.integration.test.js
@@ -1,8 +1,13 @@
 const request = require("supertest");
 const newTodo = require("../mock-data/newTodo.json");
 const app = require("../../app");
+const todoModel = require("../../model/todo");
 
 const endpointUrl = "/todos/";
+
+const mockData=[{title:'2', done:true}];
+todoModel.get = jest.fn();
+todoModel.get.mockReturnValue(mockData)
 
 describe(endpointUrl, () => {
   it("POST " + endpointUrl, async () => {
@@ -13,5 +18,18 @@ describe(endpointUrl, () => {
     expect(response.statusCode).toBe(201);
     expect(response.body.title).toBe(newTodo.title);
     expect(response.body.done).toBe(newTodo.done);
+  });
+  
+  it("Get " + endpointUrl, async () => {
+    const response = await request(app)
+      .get(endpointUrl)
+      .send();
+    
+    expect(response.statusCode).toBe(200);
+    const data=response.body;
+ 
+    expect(data).toBeTruthy();
+    expect(Array.isArray(data)).toBe(true);
+    expect(data.length).toBe(mockData.length);
   });
 });
--- tests/unit/todoController-get.test.js
+++ tests/unit/todoController-get.test.js
@@ -2,7 +2,10 @@ const todoController = require("../../controllers/todoController");
 const todoModel = require("../../model/todo");
 const httpMocks=require("node-mocks-http");
 
+
+const mockData=[{title:'1', done:true}];
 todoModel.get = jest.fn();
+todoModel.get.mockReturnValue(mockData)
 
 let req, res;
 beforeEach(()=>{
@@ -10,18 +13,22 @@ beforeEach(()=>{
   res= httpMocks.createResponse()
 })
 describe("TodoController.get", () => {
-  it("should have a createTodo function", () => {
+  it("should have a getTodo function", () => {
     expect(typeof todoController.getTodo).toBe("function");
   });
   
-  it("should call todoModel create", () => {
+  it("should call todoModel get", () => {
     todoController.getTodo(req, res);
     expect(todoModel.get).toBeCalled();
   });
   
-  it("should return  20 response code", () => {
+  it("should return  200 response code", () => {
     todoController.getTodo(req, res);
     expect(res.statusCode).toBe(200);
+    
+    const body=res._getJSONData();
+    expect(body).toBeTruthy();
+    expect(body).toMatchObject(mockData);
     
   });
   
